7660ab3c65bbca1a5920d462fad63f8a989b2901
Sushmitha_Gowda <sushmitha_gowda@example.com>
2019-08-07 13:26:37 +0530

    Built an admin interface to create product listing.  - Created a model Products with list of items.  - Created a active admin resource for products.

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @product = products(:one)
+  end
+
+  test "should get index" do
+    get products_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_product_url
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      post products_url, params: { product: { description: @product.description, main_image: @product.main_image, price: @product.price, thumb_image: @product.thumb_image, title: @product.title } }
+    end
+
+    assert_redirected_to product_url(Product.last)
+  end
+
+  test "should show product" do
+    get product_url(@product)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_product_url(@product)
+    assert_response :success
+  end
+
+  test "should update product" do
+    patch product_url(@product), params: { product: { description: @product.description, main_image: @product.main_image, price: @product.price, thumb_image: @product.thumb_image, title: @product.title } }
+    assert_redirected_to product_url(@product)
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+
+    assert_redirected_to products_url
+  end
+end
