a4c24ffd1a2f6664db7e33a39c6770a799d0de74
Sushmitha_Gowda <sushmitha_gowda@example.com>
2019-08-26 18:29:00 +0530

    Updated with uniqueness testing for product title.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,30 +4,30 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
   test "should not save product without title" do
-  	product = Product.new
-  	assert_not product.save
+    product = Product.new
+    assert_not product.save
   end
 
   test 'name must be unique' do
-  	new_product = Product.new
-  	refute new_product.valid?
+    new_product = Product.new
+    refute new_product.valid?
   end
 
   test "should not save product without price" do
-  	product = Product.new
-  	assert_not product.save
+    product = Product.new
+    assert_not product.save
   end
 
   test "should have numeric price" do
-  	product = Product.new(price: 'test')
-  	assert_not product.valid?
-  	assert_equal ["is not a number"], product.errors.messages[:price]
+    product = Product.new(price: 'test')
+    assert_not product.valid?
+    assert_equal ["is not a number"], product.errors.messages[:price]
   end
 
   test "should not save product without description" do
-  	product = Product.new
-  	assert_not  product.save
+    product = Product.new
+    assert_not  product.save
   end
-
 end
