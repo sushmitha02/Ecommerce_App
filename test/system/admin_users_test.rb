0dd1e689b9d9aac0df858a546a968cf13b6e1ffc
Sushmitha_Gowda <sushmitha_gowda@example.com>
2019-08-27 20:05:27 +0530

    Done with admin side testing. - tested foe login

---
+++ test/system/admin_users_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class AdminUsersTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit admin_users_url
+  #
+  #   assert_selector "h1", text: "AdminUser"
+  # end
+
+  test "creating a New Admin" do
+    visit admin_user_session_url
+
+    fill_in "admin_user_email", with: 'admin@example.com'
+    fill_in "admin_user_password", with: 'password'
+
+    click_on "Login"
+
+
+  end
+
+end
